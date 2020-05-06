a8912a8aeb6fa568713fb0bf705ed48fe4b75b24
tjeubaoit <anhtn.bk@gmail.com>
2020-05-06 18:30:02 +0700

    update leetcode 637

---
+++ leetcode/637_average-level-binary-tree/Solution.py
@@ -0,0 +1,30 @@
+from queue import SimpleQueue
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def averageOfLevels(self, root: TreeNode):
+        if not root: return [0]
+        q = SimpleQueue()
+        q.put(root)
+        ans = []
+        while not q.empty():
+            s, c = 0, 0
+            for i in range(0, q.qsize()):
+                node = q.get()
+                s += node.val
+                c += 1
+                if node.left: q.put(node.left)
+                if node.right: q.put(node.right)
+            ans.append(s / c)
+        return ans
+
+
+if __name__ == '__main__':
+    ret = Solution().averageOfLevels([])
