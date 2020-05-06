2b07a5ed393ac1bc88a08eed1ccd7052e3b8d1b8
tjeubaoit <anhtn.bk@gmail.com>
2020-05-06 22:16:32 +0700

    add leetcode 327

---
+++ leetcode/327_count-range-sum/Solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countRangeSum(self, nums, lower, upper):
+        sum = 0
+        i, j = 0, len(nums)-1
+        
+
+
+if __name__ == '__main__':
+    ret = Solution().countRangeSum(nums=[-2,5,-1], lower=-2, upper=2)
+    print(ret)
