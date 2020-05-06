58f1224dda1f5134192ea0ceeb1cef41b1562f68
tjeubaoit <anhtn.bk@gmail.com>
2020-05-06 18:55:50 +0700

    update leetcode 111

---
+++ leetcode/111-minimum-depth-binary-tree.py
@@ -0,0 +1,26 @@
+from queue import SimpleQueue
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if not root: return 0
+        q = SimpleQueue()
+        q.put(root)
+        dep = 0
+        while not q.empty():
+            for _ in range(0, q.qsize()):
+                node = q.get()
+                if not node.left and not node.right:
+                    return dep+1
+                if node.left:
+                    q.put(node.left)
+                if node.right:
+                    q.put(node.right)
+            dep += 1
