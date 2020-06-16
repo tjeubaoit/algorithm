5efcd45be4629221cc6076423b6cafa1e372c59e
tjeubaoit <anhtn.bk@gmail.com>
2020-06-17 01:38:12 +0700

    add leetcode 198

---
+++ leetcode/198-house-robber.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 0:
+            return 0
+        elif n == 1:
+            return nums[0]
+
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, n):
+            print(dp[i - 2], nums[i])
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
+        print(dp)
+        return dp[-1]
