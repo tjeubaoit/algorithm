2249b9971e0245490591bb1f03dbc3800fbc6ed5
tjeubaoit <anhtn.bk@gmail.com>
2020-06-23 22:35:17 +0700

    add leetcode 287

---
+++ leetcode/287-find-duplicate-number.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        # Find the intersection point of the two runners.
+        tortoise = hare = nums[0]
+        while True:
+            tortoise = nums[tortoise]
+            hare = nums[nums[hare]]
+            if tortoise == hare:
+                break
+
+        # Find the "entrance" to the cycle.
+        tortoise = nums[0]
+        while tortoise != hare:
+            tortoise = nums[tortoise]
+            hare = nums[hare]
+
+        return hare
