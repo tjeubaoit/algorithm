ee528e8460913f7e73b201896a3afc24b5071381
tjeubaoit <anhtn.bk@gmail.com>
2020-06-27 17:24:12 +0700

    Update leetcod1e 98

--- leetcode/98-validate-BST.py
+++ leetcode/98-validate-BST.py
@@ -5,22 +5,18 @@ class TreeNode:
         self.left = None
         self.right = None
 
-
 class Solution:
     def isValidBST(self, root: TreeNode) -> bool:
         if not root: return True
-        vals = []
-
-        def inOrderTraversal(node: TreeNode):
-            if node.left:
-                inOrderTraversal(node.left)
-            vals.append(node.val)
-            if node.right:
-                inOrderTraversal(node.right)
-
-        inOrderTraversal(root)
-
-        for i in range(len(vals) - 1):
-            if vals[i] >= vals[i + 1]:
+        stack, prev = [], float('-inf')
+        while stack or root:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            if prev >= root.val:
                 return False
+            prev = root.val
+            root = root.right
         return True
+        
