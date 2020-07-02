edd8f1c83f631467b7841fd86483af9cec3d57aa
tjeubaoit <anhtn.bk@gmail.com>
2020-07-02 15:20:53 +0700

    update leetcode 198+213 (improve space complexity)

--- leetcode/213-house-robber2.py
+++ leetcode/213-house-robber2.py
@@ -6,16 +6,13 @@ class Solution:
         if n == 1:
             return nums[0]
 
-        dp = [0] * n
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
+        prev, curr = nums[0], max(nums[0], nums[1])
         for i in range(2, n - 1):
-            dp[i] = max(nums[i] + dp[i - 2], dp[i - 1])
-        mx = dp[-2]
+            prev, curr = curr, max(nums[i] + prev, curr)
+        mx = curr
 
-        dp[0] = 0
-        dp[1] = nums[1]
+        prev, curr = 0, nums[1])
         for i in range(2, n):
-            dp[i] = max(nums[i] + dp[i - 2], dp[i - 1])
+            prev, curr = curr, max(nums[i] + prev, curr)
 
-        return max(mx, dp[-1])
+        return max(mx, curr)
