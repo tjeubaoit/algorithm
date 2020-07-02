f285e59632afc2420793c69a5449933cc5b3211a
tjeubaoit <anhtn.bk@gmail.com>
2020-07-02 19:19:08 +0700

    add leetcode 204

---
+++ leetcode/204-count-primes/Solution.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int countPrimes(int n) {
+        int count = 0;
+        boolean[] primes = new boolean[n];
+        for (int i = 2; i < n; i++) {
+            primes[i] = true;
+        }
+        for (int i = 2; i*i < n; i++) {
+            if (!primes[i]) continue;
+            int t = i*i;
+            while (t < n) {
+                primes[t] = false;
+                t += i;
+            }
+        }
+        for (int i = 2; i < n; i++) {
+            if (primes[i]) count += 1;
+        }
+        return count;
+    }
+
+    private boolean isPrime(int num) {
+        if (num <= 1) return false;
+        // Loop's ending condition is i * i <= num 
+        // instead of i <= sqrt(num)
+        // to avoid repeatedly calling an expensive 
+        // function sqrt().
+        for (int i = 2; i * i <= num; i++) {
+           if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
