acd8440c7d5e561ea9e3f74e8c7aead73958cf4f
anhtn25 <anh.tran12@onemount.com>
2020-10-12 14:48:38 +0700

    Add 1074 leetcode

---
+++ leetcode/1074-number-of-submatrices-that-sum-to-target.py
@@ -0,0 +1,29 @@
+from typing import List
+
+class Solution:
+    
+    def numSubmatrixSumTarget(self, matrix: List[List[int]], target: int) -> int:
+        rows, cols = len(matrix)+1, len(matrix[0])+1
+        psum = [[0]*cols for _ in range(rows)]
+        
+        for r in range(1, rows):
+            t = 0
+            for c in range(1, cols):
+                t += matrix[r-1][c-1]
+                psum[r][c] = psum[r-1][c] + t
+        # for r in range(rows):
+        #     print(psum[r])
+            
+        ans = 0
+        for r1 in range(rows-1):
+            for r2 in range(r1+1, rows):
+                d = {target: 1}
+                for c in range(1, cols):
+                    x = psum[r2][c] - psum[r1][c]
+                    if x in d: 
+                        ans += d[x]
+                    x += target
+                    d[x] = d.get(x, 0) + 1
+        return ans
+    
+    
